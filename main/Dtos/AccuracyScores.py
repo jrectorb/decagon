de6729cb6899aeaac7a18cb108f96066bde86533
Jarrid Rector-Brooks <jarridr@mparticle.com>
2020-05-08 11:50:49 -0400

    Minors

--- main/Dtos/AccuracyScores.py
+++ main/Dtos/AccuracyScores.py
@@ -2,5 +2,5 @@ class AccuracyScores:
     def __init__(self, auroc: float, auprc: float, aupk: float):
         self.auroc: float = auroc
         self.auprc: float = auprc
-        self.aupk:  float = aupk
+        self.apk:  float = apk
 
--- main/Logger/DecagonLogger.py
+++ main/Logger/DecagonLogger.py
@@ -1,6 +1,15 @@
 from .BaseLogger import BaseLogger
 from ..Utils.Config import Config
 import tensorflow as tf
+import atexit
+import os
+
+LOG_FILE_FORMAT = 'decagon_iteration_results_%d.csv'
+PERC_IDX = LOG_FILE_FORMAT.find('%')
+DOT_IDX  = LOG_FILE_FORMAT.find('.')
+
+def _closeFile(f: file) -> None:
+    f.close()
 
 class DecagonLogger(BaseLogger):
     '''
@@ -21,7 +30,10 @@ class DecagonLogger(BaseLogger):
             config.getSetting('NumIterationsPerCheckpoint')
         )
 
-        self.trainResultLogDir: str = config.getSetting('TrainIterationResultDir')
+        self.trainResultLogFile: file = self._getTrainResultFile(config)
+        self.trainResultWriter: DictWriter = self._getDictWriter()
+        self.trainResultWriter.writeheader()
+
         self.checkpointDir: str = config.getSetting('CheckpointDirectory')
 
         self.miniBatchIterator: EdgeMinibatchIterator = miniBatchIterator
@@ -30,6 +42,51 @@ class DecagonLogger(BaseLogger):
             placeholders,
             config
         )
+
+        atexit.register(_closeFile, f=self.trainResultLogFile)
+
+    def _getTrainResultFile(self, config: Config) -> file:
+        return open(self._getTrainResultFileName(config), 'w')
+
+    def _getTrainResultFileName(self, config: Config) -> str:
+        baseDir = config.getSetting('TrainIterationResultDir')
+        existingIndices = [
+            self._getFnameIdx(f)
+            for f in os.listdir(baseDir)
+            if self._isValidFname(baseDir, f)
+        ]
+
+        thisFileIdx = 0
+        if len(existingIndices) > 0:
+            thisFileIdx = max(thisFileIdx)
+
+        return LOG_FILE_FORMAT % thisFileIdx
+
+    def _getFnameIdx(self, fname: str) -> int:
+        stripPre = fname.lstrip(LOG_FILE_FORMAT[:PERC_IDX])
+        stripPost = stripPre.rstrip(LOG_FILE_FORMAT[DOT_IDX:])
+
+        return int(stripPost)
+
+    def _isValidFname(self, baseDir: str, fname: str) -> bool:
+        isFile = os.isfile(baseDir, fname)
+        isGoodPrefix = fname[:PERC_IDX] == LOG_FILE_FORMAT[:PERC_IDX]
+        isGoodPostfix = fname[DOT_IDX:] = LOG_FILE_FORMAT[DOT_IDX:]
+
+        return isFile and isGoodPrefix and isGoodPostfix
+
+    def _getDictWriter(self) -> csv.DictWriter:
+        fieldnames = [
+            'IterationNum',
+            'Loss',
+            'Latency',
+            'EdgeType',
+            'AUROC',
+            'AUPRC',
+            'APK'
+        ]
+
+        return csv.DictWriter(self.trainResultLogFile, fieldnames=fieldnames)
 
     @property
     def _shouldCheckpoint(self):
@@ -42,8 +99,11 @@ class DecagonLogger(BaseLogger):
     ) -> None:
         accuracyScores = self._computeAccuracyScores(feedDict)
 
-        self._writeResultsToFileSystem(iterationResults, accuracyScores)
-        self._writeResultsToStdout(iterationResults, accuracyScores)
+        iterRowDict = self._getCsvRowDict(iterationResults, accuracyScores)
+        iterString  = self._getString(iterationResults, accuracyScores)
+
+        self.trainResultWriter.writerow(rowDict)
+        print(iterString)
 
         if self._shouldCheckpoint:
             self._checkpointModel()
@@ -58,17 +118,44 @@ class DecagonLogger(BaseLogger):
             self.miniBatchIterator.val_edges_false
         )
 
-    def _writeResultsToFileSystem(
+    def _getCsvRowDict(
         self,
         iterationResults: DecagonTrainingIterationResults,
         accuracyScores: AccuracyScores
-    ) -> None:
-        pass
+    ) -> Dict:
+        return {
+            'IterationNum': self.numIterationsDone,
+            'Loss': iterationResults.iterationLoss,
+            'Latency': iterationResults.iterationLatency,
+            'EdgeType': iterationRresults.iterationEdgeType,
+            'AUROC': accuracyScores.auroc,
+            'AUPRC': accuracyScores.auprc,
+            'APK': accuracyScores.apk,
+        }
 
-    def _writeResultsToStdout(
+    def _getString(
         self,
         iterationResults: DecagonTrainingIterationResults,
         accuracyScores: AccuracyScores
-    ) -> None:
-        pass
+    ) -> str:
+        '''
+            IterationNum: %d
+            Loss: %f
+            Latency: %f
+            EdgeType: %s
+            AUROC: %f
+            AUPRC: %f
+            APK: %f
+
+
+
+        ''' % (
+            self.numIterationsDone,
+            iterationResults.iterationLoss,
+            iterationResults.iterationLatency,
+            iterationRresults.iterationEdgeType,
+            accuracyScores.auroc,
+            accuracyScores.auprc,
+            accuracyScores.apk,
+        )
 
