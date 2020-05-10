ab6970578a4e6df30ed0ff595747df9eb11c84ef
Jarrid Rector-Brooks <jarridr@mparticle.com>
2020-05-10 18:08:56 -0400

    Change copied decagon dir to softlink

--- main/Trainable/Decagon/decagon/data_preprocessing/DecagonCsrMatrix.py
+++
@@ -1,6 +0,0 @@
-import scipy.sparse as sp
-
-class DecagonCsrMatrix(sp.csr.csr_matrix):
-    def __init__(self, matrixType, *args, **kwargs):
-        self.matrixType = matrixType
-        super().__init__(*args, **kwargs)
